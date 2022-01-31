3aaa6937eb40f9be0deb71136a00ea0cc23d1701
nijin26 <nijinazarkpz@gmail.com>
2022-01-31 22:32:43 +0530

    Site updated

--- action.js
+++ action.js
@@ -1,10 +1,9 @@
 function changeToRed() {
   document.body.style.backgroundColor = "red";
-  document.getElementById("title").innerHTML = "Orange";
 }
 
-function changeToOrange() {
-  document.body.style.backgroundColor = "orange";
+function changeToViolet() {
+  document.body.style.backgroundColor = "violet";
 }
 
 function changeToBlue() {
