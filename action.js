1b750345e43d276830d5ac1c0ae038e3f0a13701
nijin26 <nijinazarkpz@gmail.com>
2022-01-31 22:07:21 +0530

    Project Completed

---
+++ action.js
@@ -0,0 +1,16 @@
+function changeToRed() {
+  document.body.style.backgroundColor = "red";
+  document.getElementById("title").innerHTML = "Orange";
+}
+
+function changeToOrange() {
+  document.body.style.backgroundColor = "orange";
+}
+
+function changeToBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+function changeToBlack() {
+  document.body.style.backgroundColor = "black";
+}
